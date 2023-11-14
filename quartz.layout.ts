9bf142daaf55a7cc03c75063df3551a58f78c680
Janaka <bjchathuranga@gmail.com>
2023-11-14 09:56:08 +0800

    Publish website

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -34,7 +34,7 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.Backlinks(),
   ],
 }
 
