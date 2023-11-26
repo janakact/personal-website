a7285b50cd5f9ec5abd6df500622913666d00f52
Janaka <bjchathuranga@gmail.com>
2023-11-26 17:14:35 +0800

    Publish website

--- quartz/components/GoogleAnalytics.tsx
+++ quartz/components/GoogleAnalytics.tsx
@@ -1,12 +1,12 @@
 export default function GoogleAnalytics() {
   return (<>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EN8E5269YV"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CDF5GWTFCX"></script>
         <script dangerouslySetInnerHTML={{__html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        gtag('config', 'G-EN8E5269YV');
+  gtag('config', 'G-CDF5GWTFCX');
         `}}>
         </script>
   </>
